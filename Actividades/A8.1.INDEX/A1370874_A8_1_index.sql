afc4536f7de0d40499df240fe86b53403dd89def
Mauricio Rico <mrico005@gmail.com>
2018-02-18 18:43:32 -0600

    Cambio de nombres y actividad index

---
+++ Actividades/A8.1.INDEX/A1370874_A8_1_index.sql
@@ -0,0 +1,35 @@
+SELECT * FROM orderdetails WHERE orderLineNumber = 1 and quantityOrdered > 50;
+609.20
+
+SELECT productCode FROM orderdetails WHERE orderLineNumber = 1 and quantityOrdered > 50;
+609.20
+
+SELECT orderLineNumber, count(*) FROM orderdetails WHERE orderLineNumber = 1 and quantityOrdered > 50 group by orderLineNumber;
+609.20
+
+SELECT * FROM orderdetails force index(idx_orderLineNumber) WHERE orderLineNumber = 1 and quantityOrdered > 50;
+92.5
+
+SELECT productCode FROM orderdetails force index(idx_orderLineNumber) WHERE orderLineNumber = 1 and quantityOrdered > 50;
+95.20
+
+SELECT orderLineNumber, count(*) FROM orderdetails force index(idx_orderLineNumber) WHERE orderLineNumber = 1 and quantityOrdered > 50 group by orderLineNumber;
+95.20
+
+SELECT * FROM orderdetails force index(idx_quantity_orderLineNumber) WHERE orderLineNumber = 1 and quantityOrdered > 50;
+93.41
+
+SELECT productCode FROM orderdetails force index(idx_quantity_orderLineNumber) WHERE orderLineNumber = 1 and quantityOrdered > 50;
+27.62
+
+SELECT orderLineNumber, count(*) FROM orderdetails force index(idx_quantity_orderLineNumber) WHERE orderLineNumber = 1 and quantityOrdered > 50 group by orderLineNumber;
+27.62
+
+SELECT * FROM orderdetails force index(idx_orderLineNumber_quantity) WHERE orderLineNumber = 1 and quantityOrdered > 50;
+8.01
+
+SELECT productCode FROM orderdetails force index(idx_orderLineNumber_quantity) WHERE orderLineNumber = 1 and quantityOrdered > 50;
+3.02
+
+SELECT orderLineNumber, count(*) FROM orderdetails force index(idx_orderLineNumber_quantity) WHERE orderLineNumber = 1 and quantityOrdered > 50 group by orderLineNumber;
+3.02
